c56ef802898c65645e5b727943e1cc5c1cdb812d
Valerio Corda <corda.valerio05@GMAIL.COM>
2022-01-11 10:21:17 +0100

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <div class="container">
+        <h1>
+            My library
+        </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum doloremque nemo voluptatibus accusantium vel, consequuntur qui itaque iure. Repellat incidunt numquam earum architecto exercitationem quos odit quas accusamus adipisci cupiditate.</p>
+
+        <section class="books">
+            <h2>Our books</h2>
+        </section>
+    </div>
+@endsection
