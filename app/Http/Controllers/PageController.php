0b6ccb7f5eadc9c968eb5745f2f3dd1ad0e92266
Valerio Corda <corda.valerio05@GMAIL.COM>
2022-01-11 11:55:30 +0100

    Created a variable books collegata al database books

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index(){
-        return view('home');
+        $books = Book::all();
+        return view('home', compact('books'));
+    }
+    public function contacts(){
+        return view('contacts');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,14 @@
 
 
 @section('content')
-    <div class="container">
-        <h1>
-            My library
-        </h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum doloremque nemo voluptatibus accusantium vel, consequuntur qui itaque iure. Repellat incidunt numquam earum architecto exercitationem quos odit quas accusamus adipisci cupiditate.</p>
+<div class="container">
+    <h1>
+        My library
+    </h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum doloremque nemo voluptatibus accusantium vel, consequuntur qui itaque iure. Repellat incidunt numquam earum architecto exercitationem quos odit quas accusamus adipisci cupiditate.</p>
 
-        <section class="books">
-            <h2>Our books</h2>
-        </section>
-    </div>
+    <section class="books">
+        <h2>Our books</h2>
+    </section>
+</div>
 @endsection
